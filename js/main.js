f6be8ff611e1460431eba721b46ce5ee8b88312e
Bob Ippolito <bob@redivi.com>
2013-11-19 15:15:05 -0800

    add a state manager for undo/redo

--- js/main.js
+++ js/main.js
@@ -1,47 +1,31 @@
 /*
   See http://snapsvg.io/start/
 */
+var state = new StateManager();
+var mainSvg = Snap("#svg");
 
-// First lets create our drawing surface out of existing SVG element
-// If you want to create new surface just provide dimensions
-// like s = Snap(800, 600);
-var s = Snap("#svg");
-// Lets create big circle in the middle:
-var background = s.rect(0,0,500,500);
-background.attr({
-	fill:"rgba(0,0,0,0)"
-})
-var stickHead = s.circle(250, 70, 50);
-// By default its black, lets change its attributes
-stickHead.attr({
-    fill: "#bada55",
-    stroke: "#000",
-    strokeWidth: 5
+// s will be our drawing surface
+var s = mainSvg.group();
+
+// dragArea is on top of s, so we can draw over the whole surface
+var dragArea = mainSvg.rect(0,0,500,500);
+dragArea.attr({
+    fill:"rgba(0,0,0,0)"
 });
+
+var undoButton = $('button.undo');
+// The undo button starts off disabled
+undoButton.attr('disabled', true);
+undoButton.on('click', function (event) {
+    event.preventDefault();
+    var undoCount = state.undo();
+    undoButton.attr('disabled', undoCount === 0);
+});
+
+var line = null;
 var lineStyle = {stroke: "#000", strokeWidth: 10};
-var stickBody = s.line(250, 120, 250, 295).attr(lineStyle);
-var stickLeftArm = s.line(250, 150, 250-70, 125).attr(lineStyle);
-var stickRightArm = s.line(250, 150, 250+70, 125).attr(lineStyle);
-var stickLeftLeg = s.line(250, 290, 250-90, 295+100).attr(lineStyle);
-var stickRightLeg = s.line(250, 290, 250+90, 295+100).attr(lineStyle);
-var stickPerson = s.group(stickHead, stickBody, stickLeftArm, stickRightArm, stickLeftLeg, stickRightLeg);
-// stickPerson.animate({transform: "t100 0"}, 2000);
-/*var origTransform = stickPerson.transform().local;
-stickPerson.drag(
-	function onMove(dx, dy, x, y, event) {
-        this.transform(origTransform + ['T', dx, dy].join(' '));
-    },
-    function onStart(x, y, event) {
-        origTransform = stickPerson.transform().local || ['t', 0, 0].join(' ');
-    },
-    function onEnd(x, y, event) {
-    });*/
-stickPerson.click(function () {
-	this.transform(this.transform().local + "r45" 	);
-})
-var line = null
-background.drag(
-	function onMove(dx, dy, x, y, event) {
+dragArea.drag(
+    function onMove(dx, dy, x, y, event) {
     	x = parseFloat(line.asPX("x1"));
     	y = parseFloat(line.asPX("y1"));
     	line.attr({x2: x + dx, y2: y + dy});
@@ -51,5 +35,14 @@ background.drag(
     	line.attr(lineStyle);
     },
     function onEnd(x, y, event) {
-    
+        var curLine = line.remove();
+        function undoLine() {
+            curLine.remove();
+        }
+        function redoLine() {
+            s.append(curLine);
+            return undoLine;
+        }
+        state.perform(redoLine);
+        undoButton.attr('disabled', false);
     });
