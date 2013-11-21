470fd2e648fbb2e40bb9efd091fa1e339c1c2245
Bob Ippolito <bob@redivi.com>
2013-11-21 15:57:31 -0800

    lineStyle binding

--- js/main.js
+++ js/main.js
@@ -10,11 +10,10 @@ var s = mainSvg.group();
 // dragArea is on top of s, so we can draw over the whole surface
 var dragArea = mainSvg.rect(0,0,500,500);
 dragArea.attr({
-    fill:"rgba(0,0,0,0)"
+    fill: "rgba(0,0,0,0)"
 });
 
 var line = null;
-var lineStyle = {stroke: "#000", strokeWidth: 10};
 dragArea.drag(
     function onMove(dx, dy, x, y, event) {
     	x = parseFloat(line.asPX("x1"));
@@ -22,20 +21,19 @@ dragArea.drag(
     	line.attr({x2: x + dx, y2: y + dy});
     },
     function onStart(x, y, event) {
-    	line = s.line(event.offsetX,event.offsetY,event.offsetX,event.offsetY);	
-    	line.attr(lineStyle);
+    	line = s.line(
+            event.offsetX, event.offsetY,
+            event.offsetX, event.offsetY);	
+    	line.attr(viewModel.lineStyle);
     },
     function onEnd(x, y, event) {
         state.perform(s, InsertSVG(line.remove()));
     });
 
-var pencilcolor = $("#pencil-color").on("change",function (event){
-	lineStyle.stroke = this.value;
-});
-
 function ViewModel() {
     var self = this;
     this.mainSvg = mainSvg;
+    this.lineStyle = {stroke: "#000000", strokeWidth: 10};
     this.s = s;
     this.state = state;
     this.undo = function undo() {
