5ca8544fab4129b7f6da189fde8aad00378c2743
Bob Ippolito <bob@redivi.com>
2013-12-03 17:11:01 -0800

    some firefox fixes

--- js/tools.js
+++ js/tools.js
@@ -1,9 +1,19 @@
+function fixEvent(e) {
+    if (!e.hasOwnProperty('offsetX')) {
+        var offset = $(e.target).offset();
+        e.offsetX = e.layerX - offset.left;
+        e.offsetY = e.layerY - offset.top;
+    }
+    return e;
+}
+
 function PencilTool(viewModel, style) {
     this.viewModel = viewModel;
     this.style = style;
 }
 PencilTool.prototype.line = null;
 PencilTool.prototype.onStart = function PencilTool_onStart(x, y, event) {
+    event = fixEvent(event);
     x = event.offsetX;
     y = event.offsetY;
     this.line = this.viewModel.s.path(['M', x, y].join(' '));
@@ -16,7 +26,8 @@ PencilTool.prototype.onMove = function PencilTool_onMove(dx, dy, x, y, event) {
     }
     var points = this.line.data('points');
     points.push(points[0] + dx, points[1] + dy);
-    this.line.attr('path', ['M', points[0], points[1], 'R'].concat(points).join(' '));
+    this.line.attr('path',
+        ['M', points[0], points[1], 'R'].concat(points).join(' '));
 };
 PencilTool.prototype.onEnd = function PencilTool_onEnd(x, y, event) {
     if (this.line === null) {
@@ -38,7 +49,8 @@ LineTool.prototype.onMove = function LineTool_onMove(dx, dy, x, y, event) {
     }
     var points = this.line.data('points');
     points.splice(2, 2, points[0] + dx, points[1] + dy);
-    this.line.attr('path', ['M', points[0], points[1], 'L'].concat(points).join(' '));
+    this.line.attr('path',
+        ['M', points[0], points[1], 'L'].concat(points).join(' '));
 }
 
 function MouseTool(viewModel) {
@@ -59,7 +71,7 @@ MouseTool.prototype.onMove = function MouseTool_onMove(dx, dy, x, y, event) {
     if (this.target === null) {
         return;
     }
-    this.target.attr('transform', this.origTransform + 't' + [dx, dy].join(','));
+    this.target.attr('transform', this.origTransform + 't' + [dx, dy].join(' '));
 };
 MouseTool.prototype.onEnd = function MouseTool_onEnd(x, y, event) {
     if (this.target === null) {
